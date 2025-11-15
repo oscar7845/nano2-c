87bc66a8f331049d49b2c0ba07af3db2c43ecdbc
oscar7845 <mail@oscarvonplaten.com>
2025-11-14 19:11:53 -0500

    forward_loss and sync cuda debug

--- src/cuda_check.h
+++ src/cuda_check.h
@@ -1,14 +1,16 @@
-//just to see cuda errors
+//add sync sometimes
 //TODO:
 #pragma once
 #include <cuda_runtime_api.h>
 #include <stdio.h>
 
-static inline void cuda_check_v1(const char *msg){
+static inline void nano2_cuda_check2(const char *where){
+    // force runtime error to show up
+    cudaDeviceSynchronize();
     cudaError_t e = cudaGetLastError();
     if(e != cudaSuccess){
-        printf("[cuda err] at %s: %s\n", msg, cudaGetErrorString(e));
+        fprintf(stderr,"[cuda] error %s : %s\n", where, cudaGetErrorString(e));
     }
 }
 
-#define CUDA_CHECK(msg) cuda_check_v1(msg)
+#define CUDA_CHECK(tag) nano2_cuda_check2(tag)
--- src/nano2_model.h
+++ src/nano2_model.h
@@ -1,53 +1,64 @@
-//make compiler stop crying
-//TODO: add m and v ptrs
 #pragma once
 #include <stddef.h>
 #include <stdint.h>
 
-struct Config; // i don't want to include config.h here
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Config; // don't want full include here
 
 struct Params{
-    float *E;
-    float *ln1_g, *ln1_b;
-    float *Wq,*Wk,*Wv,*Wo;
-    float *ln2_g,*ln2_b;
-    float *W1,*b1;
-    float *W2,*b2;
+  float *E;
+  float *ln1_g,*ln1_b;
+  float *Wq,*Wk,*Wv,*Wo;
+  float *ln2_g,*ln2_b;
+  float *W1,*b1;
+  float *W2,*b2;
 };
 
 struct Opt{
-    float *m;
-    float *v;
+  float *m;
+  float *v;
 };
 
 struct Buffers{
-    float *x;
-    float *x_ln1;
-    float *q,*k,*v;
-    float *scores;
-    float *probs;
-    float *attn_out;
-    float *x_res1;
-    float *x_ln2;
-    float *ff1;
-    float *ff2;
-    float *x_res2;
-    float *logits;
-    float *rowmax;
+  float *x;
+  float *x_ln1;
+  float *q,*k,*v;
+  float *scores;
+  float *probs;
+  float *attn_out;
+  float *x_res1;
+  float *x_ln2;
+  float *ff1;
+  float *ff2;
+  float *x_res2;
+  float *logits;
+  float *rowmax;
 };
 
 struct Model{
-    int B,T,D,V,F;
-    struct Params p;
-    struct Opt opt;
-    float *flat_params;
-    float *flat_grads;
-    size_t n_params;
-    float *pos_sin;
-    float *pos_cos;
-    struct Buffers buf;
+  int B,T,D,V,F;
+  struct Params p;
+  struct Opt opt;
+  float *flat_params;
+  float *flat_grads;
+  size_t n_params;
+  float *pos_sin;
+  float *pos_cos;
+  struct Buffers buf;
 };
 
-void model_init(struct Model *M, const struct Config *c);
-void model_free(struct Model *M);
-void model_log_summary(const struct Model *M, const struct Config *c);
+void  model_init (struct Model *M, const struct Config *c);
+void  model_free (struct Model *M);
+void  model_log_summary(const struct Model *M, const struct Config *c);
+
+// small fwd-loss helper (tokens on host)
+float nano2_forward_loss(struct Model *M,
+                         const uint8_t *h_x,
+                         const uint8_t *h_y);
+
+#ifdef __cplusplus
+}
+#endif
